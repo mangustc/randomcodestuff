26bb21410d5de6ad9e6fbf5ae5a1acda4235e8ae
Ivan Lifanov <letalbark@gmail.com>
2025-11-08 02:25:01 +0700

    upgrade?????????

--- rmp/ind/lib/main.dart
+++ rmp/ind/lib/main.dart
@@ -2,5 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:ind/screens/main_screen/main_screen_provider.dart';
 
 void main() {
-  runApp(MaterialApp(home: MainScreenProvider()));
+  runApp(MaterialApp(
+      home: MainScreenProvider(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Color(0xFF003366),
+        secondaryHeaderColor: Color(0xFFD4AF37),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Color(0xFF003366),
+          elevation: 2,
+          iconTheme: IconThemeData(color: Color(0xFF003366)),
+        ),
+        textTheme: TextTheme(
+          // bodyLarge: TextStyle(fontSize: 20),
+          // bodyMedium: TextStyle(fontSize: 16),
+          // bodySmall: TextStyle(fontSize: 12),
+          // displayLarge: TextStyle(fontSize: 16),
+          // displayMedium: TextStyle(fontSize: 16),
+          // displaySmall: TextStyle(fontSize: 16),
+          // headlineLarge: TextStyle(fontSize: 16),
+          // headlineMedium: TextStyle(fontSize: 16),
+          // headlineSmall: TextStyle(fontSize: 16),
+          // titleLarge: TextStyle(fontSize: 16),
+          // titleMedium: TextStyle(fontSize: 16),
+          // titleSmall: TextStyle(fontSize: 16),
+        ),
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF003366),
+          secondary: Color(0xFFD4AF37),
+          onPrimary: Colors.white,
+        ),
+      ),
+  ));
 }
--- rmp/ind/lib/screens/main_screen/list_page.dart
+++ rmp/ind/lib/screens/main_screen/list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show FilteringTextInputFormatter;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ind/screens/main_screen/list_cubit.dart';
 import 'package:ind/structures.dart';
 
@@ -14,7 +16,6 @@ int calculateAge(DateTime birthDate) {
   DateTime currentDate = DateTime.now();
   int age = currentDate.year - birthDate.year;
 
-  // Adjust age if the birthday hasn't occurred yet this year
   if (currentDate.month < birthDate.month ||
       (currentDate.month == birthDate.month && currentDate.day < birthDate.day)) {
     age--;
@@ -22,22 +23,63 @@ int calculateAge(DateTime birthDate) {
   return age;
 }
 
+class TitleContainer extends StatelessWidget {
+  final Widget child;
+
+  const TitleContainer({
+    super.key,
+    required this.child,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Theme.of(context).colorScheme.primary,
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.center,
+      child: child,
+    );
+  }
+}
 
 
 class InfoText extends StatelessWidget {
-  const InfoText({super.key, required this.name, required this.value});
-
   final String name;
   final String value;
 
+  const InfoText({
+    super.key,
+    required this.name,
+    required this.value,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(name),
-        Text(value),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 140,
+            child: Text(
+              name,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              value,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -55,20 +97,90 @@ class PersonPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(child: Column(
         children: <Widget>[
-          Image.asset(personFull.personAssetImagePath,),
-          InfoText(name: "Family name", value: personFull.personFamilyName),
-          InfoText(name: "Forename", value: personFull.personForename),
-          InfoText(name: "Gender", value: personFull.personGenderName),
-          InfoText(name: "Date of birth", value: "${personFull.personBirthDate.day}/${personFull.personBirthDate.month}/${personFull.personBirthDate.year} (${calculateAge(personFull.personBirthDate)} years old)"),
-          InfoText(name: "Nationality", value: personFull.personNationalityCountryName,),
-          InfoText(name: "WantedBy", value: personFull.personWantedByCountryName,),
-          InfoText(name: "Details", value: personFull.personDetails,),
-          InfoText(name: "Physical characteristics", value: personFull.personPhysicalCharacteristics,),
-          InfoText(name: "Charges", value: personFull.personCharges,),
+          TitleContainer(
+            child: Column(
+              children: [
+                Text(
+                  "${personFull.personFamilyName}, ${personFull.personForename}".toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: (Theme.of(context).textTheme.bodyMedium ?? TextStyle()).copyWith(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Wanted by ",
+                      ),
+                      TextSpan(
+                        text: personFull.personWantedByCountryName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )
+                      )
+                    ],
+                  )
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          Padding(
+            padding: EdgeInsets.only(left: 12, right: 12),
+            child: Column(
+              children: [
+                Image.asset(personFull.personAssetImagePath,),
+                SizedBox(height: 20,),
+                InfoText(name: "Family name", value: personFull.personFamilyName),
+                InfoText(name: "Forename", value: personFull.personForename),
+                InfoText(name: "Gender", value: personFull.personGenderName),
+                InfoText(name: "Date of birth", value: "${personFull.personBirthDate.day}/${personFull.personBirthDate.month}/${personFull.personBirthDate.year} (${calculateAge(personFull.personBirthDate)} years old)"),
+                InfoText(name: "Nationality", value: personFull.personNationalityCountryName,),
+                if (personFull.personDetails != "") InfoText(name: "Details", value: personFull.personDetails,),
+                if (personFull.personPhysicalCharacteristics != "") InfoText(name: "Physical characteristics", value: personFull.personPhysicalCharacteristics,),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Charges",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 4, bottom: 20),
+                        child: Text(
+                          "Published as provided by requesting entity",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        personFull.personCharges,
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20,),
+              ],
+            ),
+          ),
         ],
       ))
     );
-
   }
 }
 class PersonConcise extends StatelessWidget {
@@ -111,47 +223,186 @@ class _ListPageState extends State<ListPage> {
   final _currentAgeFromController = TextEditingController();
   final _currentAgeToController = TextEditingController();
   int? _selectedGender;
-  int? _selectedNationalityID;
-  int? _selectedWantedByID;
+  int? _selectedNationalityCountryID;
+  int? _selectedWantedByCountryID;
+  final _keywordsController = TextEditingController();
+
+  void _applyFilters() {
+    final filter = PersonFilter(
+        personFamilyName: _familyNameController.text.isEmpty ? null : _familyNameController.text,
+        personForename: _forenameController.text.isEmpty ? null : _forenameController.text,
+        personCurrentAgeFrom: _currentAgeFromController.text.isEmpty ? null : int.tryParse(_currentAgeFromController.text),
+        personCurrentAgeTo: _currentAgeToController.text.isEmpty ? null : int.tryParse(_currentAgeToController.text),
+        personGender: _selectedGender,
+        personNationalityCountryID: _selectedNationalityCountryID,
+        personWantedByCountryID: _selectedWantedByCountryID,
+        personKeywords: _keywordsController.text.isEmpty ? null : _keywordsController.text
+    );
+    context.read<ListCubit>().updateSearch(filter);
+  }
+
+  void _showFilterDialog(BuildContext context, List<Country> countryList) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Filters"),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Family Name'),
+                  controller: _familyNameController,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Forename'),
+                  controller: _forenameController,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'Age From'),
+                        controller: _currentAgeFromController,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'Age To'),
+                        controller: _currentAgeToController,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                DropdownButtonFormField<int>(
+                  decoration: const InputDecoration(labelText: 'Gender'),
+                  value: _selectedGender,
+                  items: [
+                    const DropdownMenuItem(value: null, child: Text('Any')),
+                    const DropdownMenuItem(value: 0, child: Text('Unknown')),
+                    const DropdownMenuItem(value: 1, child: Text('Male')),
+                    const DropdownMenuItem(value: 2, child: Text('Female')),
+                  ],
+                  onChanged: (value) {
+                    _selectedGender = value;
+                  },
+                ),
+                DropdownButtonFormField<int>(
+                  decoration: const InputDecoration(labelText: 'Nationality'),
+                  value: _selectedNationalityCountryID,
+                  items: [
+                    const DropdownMenuItem(value: null, child: Text('Any')),
+                    ...countryList.map((country) => DropdownMenuItem(
+                      value: country.countryID,
+                      child: Text(country.countryName),
+                    )),
+                  ],
+                  onChanged: (value) {
+                    _selectedNationalityCountryID = value;
+                  },
+                ),
+                DropdownButtonFormField<int>(
+                  decoration: const InputDecoration(labelText: 'Wanted By'),
+                  value: _selectedWantedByCountryID,
+                  items: [
+                    const DropdownMenuItem(value: null, child: Text('Any')),
+                    ...countryList.map((country) => DropdownMenuItem(
+                      value: country.countryID,
+                      child: Text(country.countryName),
+                    )),
+                  ],
+                  onChanged: (value) {
+                    _selectedWantedByCountryID = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Keywords'),
+                  controller: _keywordsController,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () { Navigator.of(context).pop(); },
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _applyFilters();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Apply Filters"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ind'),
+        title: SvgPicture.asset(
+          "assets/interpol.svg",
+          width: 40,
+          height: 40,
+          colorFilter: ColorFilter.mode(
+              Theme.of(context).appBarTheme.foregroundColor ?? Theme.of(context).colorScheme.primary,
+              BlendMode.srcIn
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_alt,),
+            onPressed: () {
+              final state = context.read<ListCubit>().state;
+              _showFilterDialog(context, state.countryList);
+            },
+          ),
+        ],
       ),
       body: BlocBuilder<ListCubit, ListCubitState>(
-          builder: (context, state) {
-            if (state.countryList.isEmpty) {
-              context.read<ListCubit>().updateSearch(PersonFilter());
-            }
-            return SingleChildScrollView(child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+        builder: (context, state) {
+          if (state.countryList.isEmpty) {
+            _applyFilters();
+          }
+          return SingleChildScrollView(
+            child: Column(
               children: [
-                Row(
-                  children: [
-                    Expanded(child: TextFormField(
-                      decoration: InputDecoration(labelText: "Family Name"),
-                      controller: _familyNameController,
-                      keyboardType: TextInputType.text,
-                      onChanged: (newValue) {
-                        // context.read<FormCubit>().changeA(newValue);
-                      },
-                    )),
-                  ],
+                TitleContainer(
+                  child: Text(
+                    "Wanted persons",
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
                 ),
+                SizedBox(height: 20,),
                 Wrap(
                   alignment: WrapAlignment.spaceEvenly,
                   spacing: 12,
                   runSpacing: 12,
                   children: [
-                    for (var personFull in state.personFullList)
-                      PersonConcise(personFull: personFull),
+                    for (var personFull in state.personFullList) PersonConcise(personFull: personFull),
                   ],
-                )
-              ],
-            ));
-          }
+                ),
+              ]
+            ),
+          );
+        },
       ),
     );
   }
